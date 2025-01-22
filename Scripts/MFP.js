40cf549274a62462ff3e53cdc8f5494d8a21221e
subtek <760675+SubTek@users.noreply.github.com>
2025-01-22 17:41:19 -0500

    Update MFP.js

--- Scripts/MFP.js
+++ Scripts/MFP.js
@@ -1,9 +1,12 @@
 var body = $response.body;
-var response = JSON.parse(body);
+var obj = JSON.parse(body);
 
-response["subscription_status"] = "active";
-response["plan"] = "premium";
-response["is_premium"] = true;
+obj.subscriptionEndDate = "2029-05-01T10:10:53.929Z",
+obj.subscriptionStartDate = "2022-05-01T10:10:53.929Z",
+obj.plan = "premium",
+obj.is_premium = "true"
+obj.subscription_status = "active",
+obj.hasPremium = true,
 
-body = JSON.stringify(response);
-$done({ body });
+body = JSON.stringify(obj);
+$done({body}); 
